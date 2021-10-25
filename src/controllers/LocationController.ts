5fe48a72a25a4f63bdd129b18dc89bca3ddd2f9a
matheuscorreiag <matheuscorreiags@gmail.com>
2021-10-25 03:05:36 -0300

    feat: add getLocationByCoords

--- src/controllers/LocationController.ts
+++ src/controllers/LocationController.ts
@@ -10,16 +10,34 @@ interface ICoords {
 class LocationController {
   async getAllLocations(req: Request, res: Response) {
     try {
-      return res.send();
+      const prisma = new PrismaClient();
+      prisma.location.findMany().then((locations) => {
+        return res.send(locations);
+      });
     } catch (err) {
       return res.send(400);
+    }
+  }
+  async getLocationByCoords(req: Request, res: Response) {
+    try {
+      const body = req.query;
+      const prisma = new PrismaClient();
+
+      const data = await prisma.location.findMany({
+        where: {
+          lat: Number(body.lat),
+          long: Number(body.long),
+        },
+      });
+      return res.send(data);
+    } catch (err) {
+      console.log(err);
     }
   }
   async newLocation(req: Request, res: Response) {
     try {
       const body: ICoords = req.body;
       const prisma = new PrismaClient();
-      console.log(body);
 
       if (!body.lat || !body.long || !body.message || !body.name) {
         return res.send({
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import LocationController from "./controllers/LocationController";
 const routes = express.Router();
 
 routes.get("/", LocationController.getAllLocations);
+routes.get("/getLocationByCoords", LocationController.getLocationByCoords);
 routes.post("/addMarker", LocationController.newLocation);
 
 export default routes;
